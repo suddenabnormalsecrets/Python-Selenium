9929c03bd805ae11450aa11bbdd57a247009c005
suddenlyhmabnormally <suddenlyhmabnormally@gmail.com>
2025-07-25 12:56:48 -0400

    Update test_start.py

--- PyTestScripts/test_start.py
+++ PyTestScripts/test_start.py
@@ -1,3 +1,4 @@
+
 # def test_m1():
 #     print('hello')
 #
@@ -8,3 +9,5 @@
 #
 # def test_m3():
 #     print('this is m3')
+# aws_access_key_id = AKIAUM4G6O6NMCSVOKF5  
+# aws_secret_access_key = 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
